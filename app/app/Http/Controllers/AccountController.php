172f624964418c607605cfecd17bf61519a59cff
Andre Felipe <andre_felipemv@hotmail.com>
2025-05-27 07:53:00 -0300

    Limpa código AccountController.

--- app/app/Http/Controllers/AccountController.php
+++ app/app/Http/Controllers/AccountController.php
@@ -11,62 +11,6 @@ use Illuminate\Http\Response;
 
 class AccountController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return Account::all();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreAccountRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Account $account)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Account $account)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateAccountRequest $request, Account $account)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Account $account)
-    {
-        //
-    }
-
     public function getAllAccounts() 
     {
         return AccountResource::collection(Account::all());
