a6ca7223b73db97c1431669414eb0563e4f2d813
Andre Felipe <andre_felipemv@hotmail.com>
2025-05-27 11:02:49 -0300

    Cria arquivo de testes

--- app/routes/api.php
+++ app/routes/api.php
@@ -4,21 +4,6 @@ use App\Http\Controllers\AccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-// Route::apiResource('accounts', AccountController::class);
-// Route::get('testeRota', [AccountController::class, 'testeSeApareceARota']);
-
-// Route::prefix('accounts')->group(function () {
-//     Route::apiResource('/', AccountController::class)->parameters([
-//         '' => 'account'
-//     ])->shallow(); 
-
-//     Route::get('/getBalance', [AccountController::class, 'testeSeApareceARota']);
-// });
-
 Route::fallback(function() {
     // abort(400, 'Bad Request. See endpoint List.');
     return response()->json([
--- app/routes/web.php
+++ app/routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/balance', [AccountController::class, 'getBalance']);
 Route::get('/allAccounts', [AccountController::class, 'getAllAccounts']);
 Route::post('/event', [AccountController::class, 'processEvent']);
